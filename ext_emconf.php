9037443b12d82c995da6aa5b8043ec5218dbe188
Markus Timtner <markus@timtner.com>
2024-03-25 14:10:05 +0100

    refined requirements

--- ext_emconf.php
+++ ext_emconf.php
@@ -10,39 +10,29 @@
  * writing. "version" and "dependencies" must not be touched!
  ***************************************************************/
 
-$EM_CONF[$_EXTKEY] = array (
+$EM_CONF[$_EXTKEY] = [
   'title' => 'Mtness Gridelements Configuration',
   'description' => 'Gridelements preconfig. Column grids, grids for simple accordions, tabs and content slider.',
   'category' => 'misc',
-  'author' => 'Pascal Mayer, Markus Timtner',
-  'author_email' => 'typo3@bsdist.ch, markus@timtner.tech',
+  'author' => 'Markus Timtner',
+  'author_email' => 'markus@timtner.tech',
   'author_company' => '',
   'version' => '1.5.0',
   'state' => 'stable',
   'uploadfolder' => false,
   'createDirs' => '',
-  'clearCacheOnLoad' => 1,
-  'constraints' => 
-  array (
-    'depends' => 
-    array (
+  'constraints' => [
+    'depends' => [
       'typo3' => '11.5.0-11.5.99',
       'gridelements' => '11.5.0-0.0.0',
-    ),
-    'conflicts' => 
-    array (
-    ),
-    'suggests' => 
-    array (
-    ),
-  ),
-  'autoload' => 
-  array (
-    'psr-4' => 
-    array (
+    ],
+    'conflicts' => [],
+    'suggests' => [],
+  ],
+  'autoload' => [
+    'psr-4' => [
       'MarkusTimtner\\MtGrids\\' => 'Classes',
-    ),
-  ),
-  'clearcacheonload' => true,
-);
+    ],
+  ],
+];
 
